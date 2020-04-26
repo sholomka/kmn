2e5867d2ce0402039541109d75176bb2143bd694
sholomka <sholomka@gmail.com>
2020-04-26 21:52:49 +0300

    edit index

--- index.php
+++ index.php
@@ -4,13 +4,8 @@ require __DIR__ . '/vendor/autoload.php';
 
 use App\Game;
 
-$start = microtime(true);
-
 try {
     (new Game())->start()->showResults();
 } catch (Throwable $e) {
     echo $e->getMessage() . PHP_EOL;
 }
-
-$end = 'Время выполнения скрипта: ' . round(microtime(true) - $start, 10) . ' сек.';
-echo $end . PHP_EOL;
